f78aaf1d6e96e76203970646659dde3fab780584
HuangShuhang <yhnbgfd@gmail.com>
2014-04-30 21:07:47 +0800

    -

---
+++ HuaHaoERP/Helper/Events/MyEventargs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.Helper.Events
+{
+    class MyEventArgs : EventArgs
+    {
+        private int intArgs;
+        public int IntArgs
+        {
+            get { return intArgs; }
+            set { intArgs = value; }
+        }
+
+        private string stringArgs;
+        public string StringArgs
+        {
+            get { return stringArgs; }
+            set { stringArgs = value; }
+        }
+
+        private decimal decimalArgs;
+        public decimal DecimalArgs
+        {
+            get { return decimalArgs; }
+            set { decimalArgs = value; }
+        }
+
+        private List<string> listStrArgs;
+        public List<string> ListStrArgs
+        {
+            get { return listStrArgs; }
+            set { listStrArgs = value; }
+        }
+
+    }
+}
---
+++ HuaHaoERP/View/Pages/Content1/Page_MainContent1_Popup_AddClient.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HuaHaoERP.View.Pages.Content1
+{
+    /// <summary>
+    /// Interaction logic for Page_MainContent1_Popup_AddClient.xaml
+    /// </summary>
+    public partial class Page_MainContent1_Popup_AddClient : Page
+    {
+        public Page_MainContent1_Popup_AddClient()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Commit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
