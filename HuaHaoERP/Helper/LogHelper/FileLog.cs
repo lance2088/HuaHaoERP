0f120f64227c5f73c6149830306ff5efcf84fe60
HuangShuhang <yhnbgfd@gmail.com>
2014-04-30 20:12:19 +0800

    +DB的通用方法，文件日志

--- HuaHaoERP/Helper/LogHelper/FileLog.cs
+++ HuaHaoERP/Helper/LogHelper/FileLog.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace HuaHaoERP.Helper.LogHelper
 {
-    class FileLog
+    static class FileLog
     {
+        private static string path = AppDomain.CurrentDomain.BaseDirectory + "Logs\\";
 
+        public static void Log(string log)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            FileStream fs = new FileStream(path + "Log.log", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            log = DateTime.Now + " \t| " + log + "\n";
+            sw.Write(log);
+            sw.Flush();//清空缓冲区  
+            sw.Close();//关闭流  
+            fs.Close();
+        }
     }
 }
--- HuaHaoERP/Helper/SQLite/DBHelper.cs
+++ HuaHaoERP/Helper/SQLite/DBHelper.cs
@@ -7,8 +7,161 @@ using System.Data.SQLite;
 
 namespace HuaHaoERP.Helper.SQLite
 {
-    class DBHelper
+    public class DBHelper
     {
+        private string DataSource = "Data\\Data.db";
+        private SQLiteConnection conn = new SQLiteConnection();
+        private SQLiteCommand cmd = new SQLiteCommand();
 
+        public DBHelper()
+        {
+            InitializeDbConnect();
+        }
+        public DBHelper(string Password)
+        {
+            InitializeDbConnect(Password);
+        }
+
+        /// <summary>
+        /// 初始化数据库连接
+        /// </summary>
+        private void InitializeDbConnect()
+        {
+            InitializeDbConnect("");
+        }
+        /// <summary>
+        /// 初始化数据库连接 With Password
+        /// </summary>
+        /// <param name="Password"></param>
+        private void InitializeDbConnect(string Password)
+        {
+            SQLiteConnectionStringBuilder connBuilder = new SQLiteConnectionStringBuilder();
+            connBuilder.DataSource = DataSource;
+            conn.ConnectionString = connBuilder.ToString();
+            conn.SetPassword(Password);
+            conn.Open();
+            cmd.Connection = conn;
+        }
+        /// <summary>
+        /// 回收conn
+        /// </summary>
+        private void ReleaseObject()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+
+        /// <summary>
+        /// 事务执行
+        /// </summary>
+        /// <param name="sqls"></param>
+        /// <returns></returns>
+        public bool Transaction(List<string> sqls)
+        {
+            bool flag = false;
+            SQLiteTransaction strans = conn.BeginTransaction();
+            try
+            {
+                foreach (string sql in sqls)
+                {
+                    cmd.CommandText = sql;
+                    cmd.ExecuteNonQuery();
+                }
+                strans.Commit();
+                flag = true;
+            }
+            catch (Exception)
+            {
+                strans.Rollback();
+            }
+            finally
+            {
+                ReleaseObject();
+            }
+            return flag;
+        }
+
+        /// <summary>
+        /// 单次执行语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public bool SingleExecution(string sql)
+        {
+            bool flag = false;
+            try
+            {
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+                flag = true;
+            }
+            catch(Exception)
+            {
+
+            }
+            finally
+            {
+                ReleaseObject();
+            }
+            return flag;
+        }
+
+        /// <summary>
+        /// 查询,out DataSet
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="ds"></param>
+        /// <returns></returns>
+        public bool QueryData(string sql, out DataSet ds)
+        {
+            bool flag = false;
+            ds = new DataSet();
+            SQLiteDataAdapter dAdapter = new SQLiteDataAdapter(sql, conn);
+            try
+            {
+                dAdapter.Fill(ds);
+                flag = true;
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                ReleaseObject();
+            }
+            return flag;
+        }
+
+        /// <summary>
+        /// 查询单个结果
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public bool QuerySingleResult(string sql, out object result)
+        {
+            bool flag = false;
+            result = new object();
+            try
+            {
+                cmd.CommandText = sql;
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    result = reader.GetValue(0);
+                }
+                flag = true;
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                ReleaseObject();
+            }
+            return flag;
+        }
     }
 }
