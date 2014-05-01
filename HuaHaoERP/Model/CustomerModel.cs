86b7b5b68dd0d231c21f2e5137f5bcddcbfb25b7
HuangShuhang <yhnbgfd@gmail.com>
2014-05-01 23:04:12 +0800

    CustomerModel重新排版

--- HuaHaoERP/Model/CustomerModel.cs
+++ HuaHaoERP/Model/CustomerModel.cs
@@ -8,111 +8,97 @@ namespace HuaHaoERP.Model
     class CustomerModel
     {
         private Guid guid;
+        private int id;
+        private string number;
+        private string name;
+        private string company;
+        private string address;
+        private string phone;
+        private string mobilePhone;
+        private string fax;
+        private string business;
+        private string remark;
+        private string lastOrderTime;
+        private int customerLevel;
+        private int orderQuantity;
+        private string deleteMark;
 
+        #region GETSET
         public Guid Guid
         {
             get { return guid; }
             set { guid = value; }
         }
-
-        private int id;
-
         public int Id
         {
             get { return id; }
             set { id = value; }
         }
-        private string number;
-
         public string Number
         {
             get { return number; }
             set { number = value; }
         }
-        private string name;
-
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
-        private string company;
-
         public string Company
         {
             get { return company; }
             set { company = value; }
         }
-        private string address;
-
         public string Address
         {
             get { return address; }
             set { address = value; }
         }
-        private string phone;
-
         public string Phone
         {
             get { return phone; }
             set { phone = value; }
         }
-        private string mobilePhone;
-
         public string MobilePhone
         {
             get { return mobilePhone; }
             set { mobilePhone = value; }
         }
-        private string fax;
-
         public string Fax
         {
             get { return fax; }
             set { fax = value; }
         }
-        private string business;
-
         public string Business
         {
             get { return business; }
             set { business = value; }
         }
-        private string remark;
-
         public string Remark
         {
             get { return remark; }
             set { remark = value; }
         }
-        private string lastOrderTime;
-
         public string LastOrderTime
         {
             get { return lastOrderTime; }
             set { lastOrderTime = value; }
         }
-        private int customerLevel;
-
         public int CustomerLevel
         {
             get { return customerLevel; }
             set { customerLevel = value; }
         }
-        private int orderQuantity;
-
         public int OrderQuantity
         {
             get { return orderQuantity; }
             set { orderQuantity = value; }
         }
-
-        private string deleteMark;
-
         public string DeleteMark
         {
             get { return deleteMark; }
             set { deleteMark = value; }
         }
+        #endregion
     }
 }
