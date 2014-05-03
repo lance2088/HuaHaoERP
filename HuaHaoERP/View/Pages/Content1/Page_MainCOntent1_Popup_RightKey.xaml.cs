09a34e903a5438c2c46500806f88c48e5b150d98
HuangShuhang <yhnbgfd@gmail.com>
2014-05-03 21:48:21 +0800

    -

---
+++ HuaHaoERP/View/Pages/Content1/Page_MainCOntent1_Popup_RightKey.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HuaHaoERP.View.Pages.Content1
+{
+    /// <summary>
+    /// Interaction logic for Page_MainCOntent1_Popup_RightKey.xaml
+    /// </summary>
+    public partial class Page_MainCOntent1_Popup_RightKey : Page
+    {
+        public Page_MainCOntent1_Popup_RightKey()
+        {
+            InitializeComponent();
+        }
+    }
+}
