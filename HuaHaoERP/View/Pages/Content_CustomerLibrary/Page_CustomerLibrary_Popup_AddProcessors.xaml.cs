b556f1e4593aff0440548b57231d8fc4aa6acd89
HuangShuhang <yhnbgfd@gmail.com>
2014-05-06 23:40:24 +0800

    Event文件目录整理

---
+++ HuaHaoERP/View/Pages/Content_CustomerLibrary/Page_CustomerLibrary_Popup_AddProcessors.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HuaHaoERP.View.Pages.Content_CustomerLibrary
+{
+    /// <summary>
+    /// Interaction logic for Page_CustomerLibrary_Popup_AddProcessors.xaml
+    /// </summary>
+    public partial class Page_CustomerLibrary_Popup_AddProcessors : Page
+    {
+        public Page_CustomerLibrary_Popup_AddProcessors()
+        {
+            InitializeComponent();
+        }
+    }
+}
