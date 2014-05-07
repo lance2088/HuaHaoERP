b12112c7f98b8b43d23228ae37e933cf3456732c
HuangShuhang <yhnbgfd@gmail.com>
2014-05-07 20:58:29 +0800

    删除OK

--- HuaHaoERP/View/Pages/Content_CustomerLibrary/Page_CustomerLibrary.xaml.cs
+++ HuaHaoERP/View/Pages/Content_CustomerLibrary/Page_CustomerLibrary.xaml.cs
@@ -66,7 +66,14 @@ namespace HuaHaoERP.View.Pages.Content_CustomerLibrary
                 Helper.Events.PopUpEvent.OnShowPopUp(this, new Page_CustomerLibrary_Popup_AddCustomer(data));
             }
         }
-
+        private void Button_DeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataGrid_Customer.SelectedCells.Count > 0)
+            {
+                HuaHaoERP.Model.CustomerModel data = this.DataGrid_Customer.SelectedCells[0].Item as HuaHaoERP.Model.CustomerModel;
+                Helper.Events.CustomerEvent.OnMarkDelete(this, data);
+            }
+        }
         #endregion
 
         #region Supplier 供应商
@@ -82,6 +89,10 @@ namespace HuaHaoERP.View.Pages.Content_CustomerLibrary
             Helper.Events.PopUpEvent.OnShowPopUp(this, new Page_CustomerLibrary_Popup_AddSupplier());
         }
         private void DataGrid_Supplier_Row_MouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_DeleteSupplier_Click(object sender, RoutedEventArgs e)
         {
 
         }
@@ -107,6 +118,14 @@ namespace HuaHaoERP.View.Pages.Content_CustomerLibrary
                 Helper.Events.PopUpEvent.OnShowPopUp(this, new Page_CustomerLibrary_Popup_AddStaff(data));
             }
         }
+        private void Button_DeleteStaff_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataGrid_Staff.SelectedCells.Count > 0)
+            {
+                HuaHaoERP.Model.StaffModel data = this.DataGrid_Staff.SelectedCells[0].Item as HuaHaoERP.Model.StaffModel;
+                Helper.Events.StaffEvent.OnMarkDelete(this, data);
+            }
+        }
         #endregion
 
         #region Processors 外加工商
@@ -128,7 +147,16 @@ namespace HuaHaoERP.View.Pages.Content_CustomerLibrary
                 Helper.Events.PopUpEvent.OnShowPopUp(this, new Page_CustomerLibrary_Popup_AddProcessors(data));
             }
         }
+        private void Button_DeleteProcessors_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataGrid_Processors.SelectedCells.Count > 0)
+            {
+                HuaHaoERP.Model.ProcessorsModel data = this.DataGrid_Processors.SelectedCells[0].Item as HuaHaoERP.Model.ProcessorsModel;
+                Helper.Events.ProcessorsEvent.OnMarkDelete(this, data);
+            }
+        }
         #endregion
 
+        
     }
 }
