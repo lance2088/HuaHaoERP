bc67d71dfef98b441b5536bc1c46a4856ddf732e
HuangShuhang <yhnbgfd@gmail.com>
2014-05-07 16:17:36 +0800

    -

--- HuaHaoERP/Model/ProcessorsModel.cs
+++ HuaHaoERP/Model/ProcessorsModel.cs
@@ -8,20 +8,116 @@ namespace HuaHaoERP.Model
     class ProcessorsModel
     {
         private Guid guid;
+
+        public Guid Guid
+        {
+            get { return guid; }
+            set { guid = value; }
+        }
         private string number;
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
         private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
         private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
         private string area;
+
+        public string Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
         private string phone;
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
         private string mobilePhone;
+
+        public string MobilePhone
+        {
+            get { return mobilePhone; }
+            set { mobilePhone = value; }
+        }
         private string fax;
+
+        public string Fax
+        {
+            get { return fax; }
+            set { fax = value; }
+        }
         private string business;
+
+        public string Business
+        {
+            get { return business; }
+            set { business = value; }
+        }
         private string clerk;
+
+        public string Clerk
+        {
+            get { return clerk; }
+            set { clerk = value; }
+        }
         private string openingBank;
+
+        public string OpeningBank
+        {
+            get { return openingBank; }
+            set { openingBank = value; }
+        }
         private int bankCardNo;
+
+        public int BankCardNo
+        {
+            get { return bankCardNo; }
+            set { bankCardNo = value; }
+        }
         private string bankCardName;
+
+        public string BankCardName
+        {
+            get { return bankCardName; }
+            set { bankCardName = value; }
+        }
         private string remark;
+
+        public string Remark
+        {
+            get { return remark; }
+            set { remark = value; }
+        }
         private DateTime deleteMark;
+
+        public DateTime DeleteMark
+        {
+            get { return deleteMark; }
+            set { deleteMark = value; }
+        }
         private DateTime addTime;
+
+        public DateTime AddTime
+        {
+            get { return addTime; }
+            set { addTime = value; }
+        }
     }
 }
--- HuaHaoERP/Model/StaffMOdel.cs
+++ HuaHaoERP/Model/StaffMOdel.cs
@@ -8,15 +8,81 @@ namespace HuaHaoERP.Model
     class StaffModel
     {
         private Guid guid;
+
+        public Guid Guid
+        {
+            get { return guid; }
+            set { guid = value; }
+        }
         private string number;
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
         private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
         private string jobs;
+
+        public string Jobs
+        {
+            get { return jobs; }
+            set { jobs = value; }
+        }
         private DateTime entryTime;
+
+        public DateTime EntryTime
+        {
+            get { return entryTime; }
+            set { entryTime = value; }
+        }
         private string Contact;
+
+        public string Contact1
+        {
+            get { return Contact; }
+            set { Contact = value; }
+        }
         private int iDNumber;
+
+        public int IDNumber
+        {
+            get { return iDNumber; }
+            set { iDNumber = value; }
+        }
         private string remark;
+
+        public string Remark
+        {
+            get { return remark; }
+            set { remark = value; }
+        }
         private string departureTime;
+
+        public string DepartureTime
+        {
+            get { return departureTime; }
+            set { departureTime = value; }
+        }
         private DateTime deleteMark;
+
+        public DateTime DeleteMark
+        {
+            get { return deleteMark; }
+            set { deleteMark = value; }
+        }
         private DateTime addTime;
+
+        public DateTime AddTime
+        {
+            get { return addTime; }
+            set { addTime = value; }
+        }
     }
 }
