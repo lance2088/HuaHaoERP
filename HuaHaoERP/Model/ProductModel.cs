12f9f04aab2f6664b8222265eecdad90bebb1e79
HuangShuhang <yhnbgfd@gmail.com>
2014-05-08 17:23:04 +0800

    开始做生产资料

---
+++ HuaHaoERP/Model/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.Model
+{
+    class ProductModel
+    {
+        private Guid guid;
+        private int id;
+        private string number;
+        private string name;
+        private string material;
+        private string type;
+        private string specification;
+        private string process;
+        private int pavkageNumber;
+        private string remark;
+        private DateTime deleteMark;
+        private DateTime addTime;
+    }
+}
---
+++ HuaHaoERP/Model/RawMaterialsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.Model
+{
+    class RawMaterialsModel
+    {
+        private Guid guid;
+        private int id;
+        private string number;
+        private string name;
+        private string weight;
+        private string material;
+        private string supplierNumber;
+        private string incomingDate;
+        private string sp1;
+        private string sp2;
+        private string remark;
+        private DateTime deleteMark;
+        private DateTime addTime;
+    }
+}
