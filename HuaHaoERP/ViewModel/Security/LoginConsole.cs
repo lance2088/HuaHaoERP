556c0fa7ff8b18bff462d934cbd3a479290f4fa0
HuangShuhang <yhnbgfd@gmail.com>
2014-05-18 17:38:02 +0800

    -

--- HuaHaoERP/ViewModel/Security/LoginConsole.cs
+++ HuaHaoERP/ViewModel/Security/LoginConsole.cs
@@ -13,7 +13,7 @@ namespace HuaHaoERP.ViewModel.Security
             object Permissions;//string
             string sql = "select b.Permissions "
                 + " from T_System_User a LEFT JOIN T_System_UserGroup b ON  a.UserGroup=b.ID "
-                + " where a.Name='"+UserName+"' And a.Password='"+Password+"' And a.DeleteMark is NULL";
+                + " where a.Name='" + UserName + "' And a.Password='" + Password + "' And a.DeleteMark is NULL";
             flag = new Helper.SQLite.DBHelper().QuerySingleResult(sql, out Permissions);
             if (flag)
             {
