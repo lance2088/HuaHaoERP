46f981fa28c1c84adaf3a9105b2c62a70fe1f990
Huangshuhang <yhnbgfd@vip.qq.com>
2014-05-23 14:40:16 +0800

    add file

---
+++ HuaHaoERP/View/Pages/Content_Warehouse/Page_WareHouse_RawMaterials.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HuaHaoERP.View.Pages.Content_Warehouse
+{
+    public partial class Page_Warehouse_RawMaterials : Page
+    {
+        public Page_Warehouse_RawMaterials()
+        {
+            InitializeComponent();
+        }
+    }
+}
