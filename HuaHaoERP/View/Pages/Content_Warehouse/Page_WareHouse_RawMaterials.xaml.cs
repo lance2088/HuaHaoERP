6d41597d51f31812c66e54a9d15a00064c7d926c
Homer <lijianbin0719@gravatar.com>
2014-05-24 20:12:45 +0800

    新建文件

--- HuaHaoERP/View/Pages/Content_Warehouse/Page_WareHouse_RawMaterials.xaml.cs
+++ HuaHaoERP/View/Pages/Content_Warehouse/Page_WareHouse_RawMaterials.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HuaHaoERP.ViewModel.Warehouse;
+using HuaHaoERP.Model;
 
 namespace HuaHaoERP.View.Pages.Content_Warehouse
 {
@@ -19,6 +21,13 @@ namespace HuaHaoERP.View.Pages.Content_Warehouse
         public Page_Warehouse_RawMaterials()
         {
             InitializeComponent();
+            var rawMaterials = new List<RawMaterialsDetailModel>()
+            {
+                new RawMaterialsDetailModel{
+                    Id = 1
+                }
+            };
+            DataGrid_RawMaterials.ItemsSource = rawMaterials;
         }
 
         private void Button_Commit_Click(object sender, RoutedEventArgs e)
@@ -28,7 +37,7 @@ namespace HuaHaoERP.View.Pages.Content_Warehouse
 
         private void Button_Cancel_Click(object sender, RoutedEventArgs e)
         {
-
+            Helper.Events.PopUpEvent.OnHidePopUp();
         }
     }
 }
