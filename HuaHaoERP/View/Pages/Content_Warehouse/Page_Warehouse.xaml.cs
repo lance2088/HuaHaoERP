e7874c3f26e7f6d7095aa6515cede202d0739a84
Homer <lijianbin0719@gravatar.com>
2014-05-24 15:33:37 +0800

    新增俩文件

--- HuaHaoERP/View/Pages/Content_Warehouse/Page_Warehouse.xaml.cs
+++ HuaHaoERP/View/Pages/Content_Warehouse/Page_Warehouse.xaml.cs
@@ -19,8 +19,15 @@ namespace HuaHaoERP.View.Pages.Content_Warehouse
         public Page_Warehouse()
         {
             InitializeComponent();
+            InitPage();
         }
 
+        private void InitPage()
+        {
+            #region 余料管理
+            Button_Clear_Click(this, null);
+            #endregion 
+        }
         private void Button_Commit_Click(object sender, RoutedEventArgs e)
         {
 
@@ -28,7 +35,10 @@ namespace HuaHaoERP.View.Pages.Content_Warehouse
 
         private void Button_Clear_Click(object sender, RoutedEventArgs e)
         {
-
+            DatePicker_Date.Text = DateTime.Now.ToShortDateString();
+            ComboBox_Name.ItemsSource = null;
+            TextBox_Number.Text = "";
+            TextBox_Remark.Text = "";
         }
     }
 }
---
+++ HuaHaoERP/ViewModel/Warehouse/ScrapConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace HuaHaoERP.ViewModel.Warehouse
+{
+    class ScrapConsole
+    {
+        internal bool Add(Model.AssemblyLineModuleProcessModel d)
+        {
+            bool flag = true;
+            string sql = "Insert into T_PM_ProductionSchedule(Guid,Date,StaffID,ProductID,Process,Number) "
+                        + "values('" + d.Guid + "','" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "','" + d.StaffID + "','" + d.ProductID + "','" + d.Process + "'," + d.Quantity + ")";
+            flag = new Helper.SQLite.DBHelper().SingleExecution(sql);
+            return flag;
+        }
+    }
+}
