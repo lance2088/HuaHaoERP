ce12773b242f3080affab611ab30931192e2623a
Huangshuhang <yhnbgfd@vip.qq.com>
2014-05-25 18:28:10 +0800

    工龄OK

--- HuaHaoERP/Helper/Tools/Seniority.cs
+++ HuaHaoERP/Helper/Tools/Seniority.cs
@@ -10,6 +10,19 @@ namespace HuaHaoERP.Helper.Tools
     /// </summary>
     static class Seniority
     {
-
+        public static string SeniorityForMonth(DateTime Date)
+        {
+            TimeSpan TSDate = new TimeSpan(Date.Ticks);
+            TimeSpan TSDateNow = new TimeSpan(DateTime.Now.Date.Ticks);
+            TimeSpan Ts = TSDate.Subtract(TSDateNow).Duration();
+            return (Ts.Days/30).ToString();
+        }
+        public static string SeniorityForMonth(DateTime Date, DateTime Date2)
+        {
+            TimeSpan TSDate = new TimeSpan(Date.Ticks);
+            TimeSpan TSDateNow = new TimeSpan(Date2.Ticks);
+            TimeSpan Ts = TSDate.Subtract(TSDateNow).Duration();
+            return (Ts.Days / 30).ToString();
+        }
     }
 }
