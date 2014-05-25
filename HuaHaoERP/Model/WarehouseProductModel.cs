654ed20649a3380e5ebd3c05a79e57dee5eacf34
Homer <lijianbin0719@gravatar.com>
2014-05-25 15:56:41 +0800

    新增两文件

---
+++ HuaHaoERP/Model/WarehouseProductModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.Model
+{
+    class WarehouseProductModel
+    {
+        private int id;
+        private string productID;
+        private string date;
+        private string optor;
+        private string number;
+        private string remark;
+
+        public string Remark
+        {
+            get { return remark; }
+            set { remark = value; }
+        }
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public string Operator
+        {
+            get { return optor; }
+            set { optor = value; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+}
