75c8e8d39fed4298ac9397e4c404f8a1ba772ea8
Homer <lijianbin0719@gravatar.com>
2014-06-03 16:41:57 +0800

    写完类

--- HuaHaoERP/Helper/DataDefinition/ENUM.cs
+++ HuaHaoERP/Helper/DataDefinition/ENUM.cs
@@ -7,6 +7,13 @@ namespace HuaHaoERP.Helper.DataDefinition
 {
     class ENUM
     {
-
+        public enum ENUM_PERMISSIONS
+        {
+            仓库记录员 = 0,
+            流水线记录员员 = 1,
+            软件记录员 = 2,
+            管理员 = 8,
+            超级管理员 = 9
+        }
     }
 }
--- HuaHaoERP/ViewModel/Settings/UserConsole.cs
+++ HuaHaoERP/ViewModel/Settings/UserConsole.cs
@@ -2,10 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using HuaHaoERP.Model;
+using System.Data;
 
 namespace HuaHaoERP.ViewModel.Settings
 {
     class UserConsole
     {
+        internal bool Add(UserModel d)
+        {
+            bool flag = true;
+            string sql = "Insert Into T_System_User (Name,Password,UserGroup,RealName,Permissions,Remark) "
+                       + " values('" + d.Username + "','" + d.Password + "',3,'" + d.Realname + "','" + d.Permissions + "','" + d.Remark + "')";
+            flag = new Helper.SQLite.DBHelper().SingleExecution(sql);
+            return flag;
+        }
+
+        internal bool Update(UserModel d)
+        {
+            bool flag = true;
+            string sql = "update T_System_User set Password = '" + d.Password + "',RealName='" + d.Realname + "',Permissions='" + d.Permissions + "',Remark='" + d.Remark + "' where id = " + d.Id;
+            flag = new Helper.SQLite.DBHelper().SingleExecution(sql);
+            return flag;
+        }
+
+        internal bool MarkDelete(UserModel d)
+        {
+            bool flag = true;
+            string sql = "Update T_System_User Set DeleteMark='" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "' Where id=" + d.Id + "";
+            flag = new Helper.SQLite.DBHelper().SingleExecution(sql);
+            return flag;
+        }
+
+        internal bool ReadList(out List<UserModel> data)
+        {
+            bool flag = true;
+            data = new List<UserModel>();
+            string sql = "select * from T_UserInfo_Customer Where DeleteMark is null";
+            DataSet ds = new DataSet();
+            flag = new Helper.SQLite.DBHelper().QueryData(sql, out ds);
+            if (flag)
+            {
+                int id = 1;
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    UserModel d = new UserModel();
+                    d.Id = Int32.Parse(dr["ID"].ToString());
+                    d.Username = dr["NAME"].ToString();
+                    d.Password = dr["PASSWORD"].ToString();
+                    d.Realname = dr["REALNAME"].ToString();
+                    d.Permissions = Int32.Parse(dr["PERMISSIONS"].ToString());
+                    d.Remark = dr["REMARK"].ToString();
+                    data.Add(d);
+                }
+            }
+            return flag;
+        }
     }
 }
