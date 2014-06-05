7ee7b7b4f499b5a01777d050965d4f5793357ca7
HuangShuhang <yhnbgfd@gmail.com>
2014-06-05 19:45:09 +0800

    开始做License dll

--- HuaHaoERP/View/Pages/Content_Others/Page_About.xaml.cs
+++ HuaHaoERP/View/Pages/Content_Others/Page_About.xaml.cs
@@ -16,10 +16,25 @@ namespace HuaHaoERP.View.Pages.Content_Others
 {
     public partial class Page_About : Page
     {
+        private string FileName;
+
         public Page_About()
         {
             InitializeComponent();
             this.Label_Version.Content += Application.ResourceAssembly.GetName().Version.ToString();
+        }
+
+        private void Button_ImportLicense_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog open = new Microsoft.Win32.OpenFileDialog();
+            open.Title = "请选择要导入的License文件";
+            open.Filter = "石蚁License文件|*.license";
+            open.RestoreDirectory = true;
+            if ((bool)open.ShowDialog().GetValueOrDefault())
+            {
+                FileName = open.FileName;
+
+            }
         }
     }
 }
