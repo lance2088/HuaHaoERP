faea25cb0cd19164e251ec0ee919e285c2d2db81
HuangShuhang <yhnbgfd@gmail.com>
2014-06-13 15:30:10 +0800

    更详细的exe版本信息

--- HuaHaoERP/Properties/AssemblyInfo.cs
+++ HuaHaoERP/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HuaHaoERP")]
-[assembly: AssemblyDescription("华浩ERP")]
+[assembly: AssemblyTitle("Pyramid ERP")]
+[assembly: AssemblyDescription("Pyramid ERP")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("StoneAnt")]
-[assembly: AssemblyProduct("HuaHaoERP")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Pyramid ERP")]
+[assembly: AssemblyCopyright("Copyright ©  2014 StoneAnt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
