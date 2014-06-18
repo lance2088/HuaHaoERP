75c49f8fc50c3645115b8f4940cb1863e1eeeb2c
HuangShuhang <yhnbgfd@gmail.com>
2014-06-18 20:55:07 +0800

    +获取系统信息class

---
+++ HuaHaoERP/Helper/Tools/ComputerInfo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace HuaHaoERP.Helper.Tools
+{
+    internal class ComputerInfo
+    {
+        internal List<string> Macs
+        {
+            get 
+            {
+                List<string> macs = new List<string>();
+                try
+                {
+                    string mac = "";
+                    ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                    ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        if ((bool)mo["IPEnabled"])
+                        {
+                            mac = mo["MacAddress"].ToString();
+                            if (!mac.StartsWith("00"))
+                            {
+                                macs.Add(mac);
+                            }
+                        }
+                    }
+                    moc = null;
+                    mc = null;
+                }
+                catch
+                {
+                }
+                return macs;
+            }
+        }
+
+        internal string CpuID
+        {
+            get
+            {
+                try
+                {
+                    string cpuInfo = "";//cpu序列号   
+                    ManagementClass mc = new ManagementClass("Win32_Processor");
+                    ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        cpuInfo = mo.Properties["ProcessorId"].Value.ToString();
+                    }
+                    moc = null;
+                    mc = null;
+                    return cpuInfo;
+                }
+                catch
+                {
+                    return "unknow";
+                }
+                finally
+                {
+                }  
+            }
+        }
+
+        internal List<string> DiskSerialNumber
+        {
+            get
+            {
+                List<string> ids = new List<string>();
+                try
+                {
+                    String HDid = "";
+                    ManagementClass mc = new ManagementClass("Win32_DiskDrive");
+                    ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        HDid = (string)mo.Properties["SerialNumber"].Value;
+                        ids.Add(HDid);
+                    }
+                    moc = null;
+                    mc = null;
+                    return ids;
+                }
+                catch
+                {
+                    return ids;
+                }
+                finally
+                {
+                }  
+            }
+        }
+    }
+}
