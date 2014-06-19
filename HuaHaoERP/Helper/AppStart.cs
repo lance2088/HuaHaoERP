793e78931e16bc3d4ba74c4042f63a24d1b30772
HuangShuhang <yhnbgfd@gmail.com>
2014-06-19 11:07:53 +0800

    强化许可验证流程

--- HuaHaoERP/Helper/AppStart.cs
+++ HuaHaoERP/Helper/AppStart.cs
@@ -15,15 +15,7 @@ namespace HuaHaoERP.Helper
             {
                 Helper.DataDefinition.CommonParameters.DbPassword = DBPassword;
             }
-            if(File.Exists(AppDomain.CurrentDomain.BaseDirectory + "License.key"))
-            {
-                new Helper.License.FillLicense().Fill(AppDomain.CurrentDomain.BaseDirectory + "License.key");
-            }
-            else
-            {
-                Helper.DataDefinition.CommonParameters.PeriodOfValidity = -1;
-                Helper.DataDefinition.CommonParameters.LicenseModel = new StoneAnt.License.Model.LicenseModel();
-            }
+            new Helper.License.FillLicense().Fill(AppDomain.CurrentDomain.BaseDirectory + "License.key");
             if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "Background"))
             {
                 Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Background");
--- HuaHaoERP/Helper/License/FillLicense.cs
+++ HuaHaoERP/Helper/License/FillLicense.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace HuaHaoERP.Helper.License
 {
@@ -9,32 +10,46 @@ namespace HuaHaoERP.Helper.License
     {
         public void Fill(string LicenseFile)
         {
-            Helper.DataDefinition.CommonParameters.PeriodOfValidity = -1;
+            string KeyInDB = "";
             Helper.DataDefinition.CommonParameters.LicenseModel = new StoneAnt.License.Model.LicenseModel();
-
-            StoneAnt.License.Model.LicenseModel m = new StoneAnt.License.Model.LicenseModel();
-            if (new StoneAnt.License.Verify.Term().VerfyLicense(LicenseFile, out m))
+            
+            if (new ViewModel.Security.LicenseConsole().ReadKeyFromDB(out KeyInDB))//数据库存在key
             {
-                string KeyInDB = "";
-                if(new ViewModel.Security.LicenseConsole().ReadKeyFromDB(out KeyInDB))
+                if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "License.key"))//存在license文件
                 {
-                    if (KeyInDB == m.Key)
+                    StoneAnt.License.Model.LicenseModel m = new StoneAnt.License.Model.LicenseModel();
+                    if (new StoneAnt.License.Verify.Term().VerfyLicense(LicenseFile, out m))//验证License文件
                     {
-                        if(new PCRegister().CheckRegistrationInformation())
+                        if (KeyInDB == m.Key)//验证License文件与数据库Key
                         {
-                            Helper.DataDefinition.CommonParameters.LicenseModel = m;
-                            CalculatePeriodOfValidity();
+                            if (new PCRegister().CheckRegistrationInformation())//验证系统信息
+                            {
+                                Helper.DataDefinition.CommonParameters.LicenseModel = m;
+                                CalculatePeriodOfValidity();
+                                return;
+                            }
+                            else//许可匹配，电脑不匹配
+                            {
+                                System.Windows.MessageBox.Show("欢迎试用石蚁科技ERP产品，请支持正版。\n试用版请使用管理员帐号登陆。", "代码：004");
+                            }
                         }
-                        else
+                        else//许可与DB数据不匹配
                         {
-                            Helper.DataDefinition.CommonParameters.IsLockAdminLogin = true;
-                            System.Windows.MessageBox.Show("欢迎试用石蚁科技ERP产品，请支持正版。\n试用版请使用管理员帐号登陆。", "");
+                            System.Windows.MessageBox.Show("许可有误，请联系开发商，错误代码：003", "错误");
                         }
-                        return;
+                    }
+                    else//许可验证不通过
+                    {
+                        System.Windows.MessageBox.Show("许可损坏，请联系开发商，错误代码：002", "错误");
                     }
                 }
+                else//找不到许可文件
+                {
+                    System.Windows.MessageBox.Show("许可丢失，请联系开发商，错误代码：001", "错误");
+                }
+                Helper.DataDefinition.CommonParameters.IsLockAdminLogin = true;
+                Helper.DataDefinition.CommonParameters.PeriodOfValidity = -1;
             }
-            System.Windows.MessageBox.Show("许可有误，请联系开发商，错误代码：001", "错误");
         }
 
         /// <summary>
