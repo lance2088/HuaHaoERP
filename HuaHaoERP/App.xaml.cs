375fe2635b4305bd8e435982cff652bea77b1e59
HuangShuhang <yhnbgfd@gmail.com>
2014-06-27 15:19:55 +0800

    -

--- HuaHaoERP/App.xaml.cs
+++ HuaHaoERP/App.xaml.cs
@@ -40,7 +40,7 @@ namespace HuaHaoERP
                 stringBuilder.AppendFormat("/n {0}", e.Exception.InnerException.Message);
             }
             stringBuilder.AppendFormat("/n {0}", e.Exception.StackTrace);
-            MessageBox.Show(stringBuilder.ToString());
+            MessageBox.Show("应用程序出现了未捕获的异常，请联系开发商。");
             Helper.LogHelper.FileLog.ErrorLog(stringBuilder.ToString());
             e.Handled = true;
         }  
