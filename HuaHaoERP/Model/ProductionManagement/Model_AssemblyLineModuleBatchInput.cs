e67332d7d15c956103baeeee9542ef2eb3b60eaf
HuangShuhang <yhnbgfd@gmail.com>
2014-06-28 11:25:18 +0800

    +file

---
+++ HuaHaoERP/Model/ProductionManagement/Model_AssemblyLineModuleBatchInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace HuaHaoERP.Model.ProductionManagement
+{
+    class Model_AssemblyLineModuleBatchInput : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
