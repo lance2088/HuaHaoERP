1ce9df85fe7b01d999fc31f792534180677919ad
HuangShuhang <yhnbgfd@gmail.com>
2014-06-28 11:57:34 +0800

    +

--- HuaHaoERP/Model/ProductionManagement/Model_AssemblyLineModuleBatchInput.cs
+++ HuaHaoERP/Model/ProductionManagement/Model_AssemblyLineModuleBatchInput.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.ComponentModel;
 
 namespace HuaHaoERP.Model.ProductionManagement
@@ -15,7 +12,76 @@ namespace HuaHaoERP.Model.ProductionManagement
             get { return id; }
             set { id = value; }
         }
+        private Guid productGuid;
 
+        public Guid ProductGuid
+        {
+            get { return productGuid; }
+            set { productGuid = value; NotifyPropertyChanged("ProductGuid"); }
+        }
+        private string produtcNumber;
+
+        public string ProdutcNumber
+        {
+            get { return produtcNumber; }
+            set { produtcNumber = value; NotifyPropertyChanged("ProdutcNumber"); }
+        }
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; NotifyPropertyChanged("ProductName"); }
+        }
+        private string process;
+
+        public string Process
+        {
+            get { return process; }
+            set { process = value; NotifyPropertyChanged("Process"); }
+        }
+        private Guid staffGuid;
+
+        public Guid StaffGuid
+        {
+            get { return staffGuid; }
+            set { staffGuid = value; NotifyPropertyChanged("StaffGuid"); }
+        }
+        private string staffNumber;
+
+        public string StaffNumber
+        {
+            get { return staffNumber; }
+            set { staffNumber = value; NotifyPropertyChanged("StaffNumber"); }
+        }
+        private string staffName;
+
+        public string StaffName
+        {
+            get { return staffName; }
+            set { staffName = value; NotifyPropertyChanged("StaffName"); }
+        }
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; NotifyPropertyChanged("Quantity"); }
+        }
+        private int injure;
+
+        public int Injure
+        {
+            get { return injure; }
+            set { injure = value; NotifyPropertyChanged("Injure"); }
+        }
+        private string remark;
+
+        public string Remark
+        {
+            get { return remark; }
+            set { remark = value; NotifyPropertyChanged("Remark"); }
+        }
 
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
--- HuaHaoERP/View/Pages/Content_ProductionManagement/Page_ProductionManagement_AssemblyLineModuleBatchInput.xaml.cs
+++ HuaHaoERP/View/Pages/Content_ProductionManagement/Page_ProductionManagement_AssemblyLineModuleBatchInput.xaml.cs
@@ -11,11 +11,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HuaHaoERP.Model.ProductionManagement;
+using HuaHaoERP.ViewModel.ProductionManagement;
+using System.Collections.ObjectModel;
 
 namespace HuaHaoERP.View.Pages.Content_ProductionManagement
 {
     public partial class Page_ProductionManagement_AssemblyLineModuleBatchInput : Page
     {
+        ObservableCollection<Model_AssemblyLineModuleBatchInput> data = new ObservableCollection<Model_AssemblyLineModuleBatchInput>();
+
         public Page_ProductionManagement_AssemblyLineModuleBatchInput()
         {
             InitializeComponent();
@@ -24,7 +29,11 @@ namespace HuaHaoERP.View.Pages.Content_ProductionManagement
 
         private void InitializeDataGrid()
         {
-            
+            for (int i = 0; i < 20; i++)
+            {
+                data.Add(new Model_AssemblyLineModuleBatchInput { Id = i + 1 });
+            }
+            this.DataGrid_BatchInput.ItemsSource = data;
         }
 
         private void Button_Commit_Click(object sender, RoutedEventArgs e)
@@ -35,6 +44,50 @@ namespace HuaHaoERP.View.Pages.Content_ProductionManagement
         private void Button_Cancel_Click(object sender, RoutedEventArgs e)
         {
             Helper.Events.PopUpEvent.OnHidePopUp();
+        }
+
+        private void DataGrid_BatchInput_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string Header = DataGrid_BatchInput.SelectedCells[0].Column.Header.ToString();
+                if (Header == "产品编号")
+                {
+                    e.Handled = true;
+                    DataGrid_BatchInput.CurrentCell = new DataGridCellInfo(DataGrid_BatchInput.SelectedCells[0].Item, DataGrid_BatchInput.Columns[2]);
+                }
+                else if (Header == "工序")
+                {
+                    e.Handled = true;
+                    DataGrid_BatchInput.CurrentCell = new DataGridCellInfo(DataGrid_BatchInput.SelectedCells[0].Item, DataGrid_BatchInput.Columns[3]);
+                }
+                else if (Header == "员工编号")
+                {
+                    e.Handled = true;
+                    DataGrid_BatchInput.CurrentCell = new DataGridCellInfo(DataGrid_BatchInput.SelectedCells[0].Item, DataGrid_BatchInput.Columns[5]);
+                }
+                else if (Header == "数量")
+                {
+                    e.Handled = true;
+                    DataGrid_BatchInput.CurrentCell = new DataGridCellInfo(DataGrid_BatchInput.SelectedCells[0].Item, DataGrid_BatchInput.Columns[6]);
+                }
+                else if (Header == "损坏")
+                {
+                    DataGrid_BatchInput.CurrentCell = new DataGridCellInfo(DataGrid_BatchInput.SelectedCells[0].Item, DataGrid_BatchInput.Columns[0]);
+                }
+                else
+                {
+                    e.Handled = true;
+                    DataGrid_BatchInput.CurrentCell = new DataGridCellInfo(DataGrid_BatchInput.SelectedCells[0].Item, DataGrid_BatchInput.Columns[0]);
+                }
+                DataGrid_BatchInput.SelectedCells.Clear();
+                DataGrid_BatchInput.SelectedCells.Add(DataGrid_BatchInput.CurrentCell);
+            }
+        }
+
+        private void DataGrid_BatchInput_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
         }
     }
 }
