1ef5d6f1f1470e0bdb5774d78f7f569a3ae7ae2c
HuangShuhang <yhnbgfd@gmail.com>
2014-06-28 14:56:15 +0800

    简单测试了没问题

--- HuaHaoERP/ViewModel/ProductionManagement/AssemblyLineModuleBatchInputConsole.cs
+++ HuaHaoERP/ViewModel/ProductionManagement/AssemblyLineModuleBatchInputConsole.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using HuaHaoERP.Model.ProductionManagement;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data;
-using HuaHaoERP.Model.ProductionManagement;
 using System.Collections.ObjectModel;
+using System.Data;
 
 namespace HuaHaoERP.ViewModel.ProductionManagement
 {
