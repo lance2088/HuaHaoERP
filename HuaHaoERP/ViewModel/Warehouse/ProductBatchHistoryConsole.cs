78353de2ffe42c6f22a45ab61f23f1f842a36533
HuangShuhang <yhnbgfd@gmail.com>
2014-07-01 20:13:03 +0800

    +file

---
+++ HuaHaoERP/ViewModel/Warehouse/ProductBatchHistoryConsole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.ViewModel.Warehouse
+{
+    class ProductBatchHistoryConsole
+    {
+
+    }
+}
