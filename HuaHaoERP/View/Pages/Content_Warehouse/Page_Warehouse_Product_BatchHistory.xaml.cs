f0c1e72e7a8860e97020701ec696384bc37ec161
HuangShuhang <yhnbgfd@gmail.com>
2014-07-02 21:24:58 +0800

    仓库批量记录可以看了

--- HuaHaoERP/View/Pages/Content_Warehouse/Page_Warehouse_Product_BatchHistory.xaml.cs
+++ HuaHaoERP/View/Pages/Content_Warehouse/Page_Warehouse_Product_BatchHistory.xaml.cs
@@ -1,24 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using HuaHaoERP.Model.Order;
+using HuaHaoERP.ViewModel.Orders;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HuaHaoERP.View.Pages.Content_Warehouse
 {
     public partial class Page_Warehouse_Product_BatchHistory : Page
     {
+        ObservableCollection<Model_BatchInputOrder> data = new ObservableCollection<Model_BatchInputOrder>();
+
         public Page_Warehouse_Product_BatchHistory()
         {
             InitializeComponent();
+            InitializeDataGrid();
+        }
+
+        private void InitializeDataGrid()
+        {
+            new BatchInputOrderConsole().ReadOrder(out data);
+            this.DataGrid_BatchHistory.ItemsSource = data;
         }
 
         private void Button_Cancel_Click(object sender, RoutedEventArgs e)
---
+++ HuaHaoERP/ViewModel/Orders/BatchInputOrderConsole.cs
@@ -0,0 +1,33 @@
+﻿using HuaHaoERP.Helper.SQLite;
+using HuaHaoERP.Model.Order;
+using System;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace HuaHaoERP.ViewModel.Orders
+{
+    class BatchInputOrderConsole
+    {
+        internal void ReadOrder(out ObservableCollection<Model_BatchInputOrder> data)
+        {
+            data = new ObservableCollection<Model_BatchInputOrder>();
+            Model_BatchInputOrder m;
+            DataSet ds = new DataSet();
+            string TableName = "T_Warehouse_ProductBatchInput";
+            string sql = "select * from " + TableName + " WHERE DeleteMark ISNULL";
+            if (new DBHelper().QueryData(sql, out ds))
+            {
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    m = new Model_BatchInputOrder();
+                    m.Guid = (Guid)dr["Guid"];
+                    m.Number = dr["Number"].ToString();
+                    m.Name = dr["Name"].ToString();
+                    m.Date = Convert.ToDateTime(dr["Date"].ToString()).ToString("yyyy-MM-dd");
+                    m.Remark = dr["Remark"].ToString();
+                    data.Add(m);
+                }
+            }
+        }
+    }
+}
