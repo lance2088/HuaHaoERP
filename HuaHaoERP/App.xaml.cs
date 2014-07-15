77eb6529dd4a1db77e84f51ea8e9f4376923880b
HuangShuhang <yhnbgfd@gmail.com>
2014-07-15 10:57:53 +0800

    =

--- HuaHaoERP/App.xaml.cs
+++ HuaHaoERP/App.xaml.cs
@@ -5,24 +5,17 @@ using System.Windows.Threading;
 
 namespace HuaHaoERP
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            // Get Reference to the current Process
             Process thisProc = Process.GetCurrentProcess();
-            // Check how many total processes have the same name as the current one
             if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
             {
-                // If ther is more than one, than it is already running.
                 MessageBox.Show("金字塔ERP已经运行。");
                 Application.Current.Shutdown();
                 return;
             }
-
             base.OnStartup(e);
         }
 
