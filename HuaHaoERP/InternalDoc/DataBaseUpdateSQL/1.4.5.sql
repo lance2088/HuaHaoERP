eb83720aae0934a994197b3305abba2ea1869fc5
Homer <lijianbin0719@gravatar.com>
2014-07-15 19:08:13 +0800

    修改更新SQL的错别字

--- HuaHaoERP/InternalDoc/DataBaseUpdateSQL/1.4.5.sql
+++ HuaHaoERP/InternalDoc/DataBaseUpdateSQL/1.4.5.sql
@@ -1,6 +1,6 @@
-﻿Update T_PM_ProductionSchedule SET Process='圆片' WHere Process='冲板';
+﻿Update T_PM_ProductionSchedule SET Process='圆片' WHere Process='冲版';
 Update T_PM_ProductionSchedule SET Process='液压' WHere Process='拉伸';
-Update T_ProductInfo_Product SET P1='圆片' WHere P1='冲板';
+Update T_ProductInfo_Product SET P1='圆片' WHere P1='冲版';
 Update T_ProductInfo_Product SET P2='液压' WHere P2='拉伸';
 Insert Into T_System_Settings(ID,"Key",Value,Remark) Values(2,'DBVersion','1.4.5','Insert');
 alter TABLE T_PM_ProcessBatchInput ADD "ProcessorsID" Guid;
