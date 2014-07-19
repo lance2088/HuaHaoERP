cc15a10c08e25f1daec001690a366e1f4d6a592e
Huangshuhang <yhnbgfd@vip.qq.com>
2014-07-20 00:51:46 +0800

    =

--- HuaHaoERP/Helper/SQLite/DBCreate.cs
+++ HuaHaoERP/Helper/SQLite/DBCreate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-
-namespace HuaHaoERP.Helper.SQLite
+﻿namespace HuaHaoERP.Helper.SQLite
 {
     class DBCreate
     {
