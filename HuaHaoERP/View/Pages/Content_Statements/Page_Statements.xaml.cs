150bf28f0d16099d53dc0acc201589abbc52e8f4
HuangShuhang_i5 <yhnbgfd@gmail.com>
2014-08-22 20:35:02 +0800

    new branches：Chart

--- HuaHaoERP/View/Pages/Content_Statements/Page_Statements.xaml.cs
+++ HuaHaoERP/View/Pages/Content_Statements/Page_Statements.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace HuaHaoERP.View.Pages.Content_Statements
 {
@@ -19,6 +7,7 @@ namespace HuaHaoERP.View.Pages.Content_Statements
         public Page_Statements()
         {
             InitializeComponent();
+            this.Frame_Production.Content = new Page_Statements_Production();
         }
     }
 }
---
+++ HuaHaoERP/View/Pages/UserControl_Chart/UserControl_ChartTest.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HuaHaoERP.View.Pages.UserControl_Chart
+{
+    public partial class UserControl_ChartTest : UserControl
+    {
+        public UserControl_ChartTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
