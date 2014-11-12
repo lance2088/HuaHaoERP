5e7a1df9fd6e023bd030ec9bb913a124468a7922
HuangShuhang_i5 <yhnbgfd@gmail.com>
2014-11-12 22:03:48 +0800

    =

--- HuaHaoERP/InternalDoc/DataBaseUpdateSQL/1.6.3.sql
+++ HuaHaoERP/InternalDoc/DataBaseUpdateSQL/1.6.3.sql
@@ -1,2 +1,16 @@
-
+CREATE TABLE T_ProductInfo_Wafer (
+"Guid"  Guid NOT NULL,
+PRIMARY KEY ("Guid" ASC)
+)
+;
+CREATE TABLE T_Order_Wafer (
+"Guid"  Guid NOT NULL,
+PRIMARY KEY ("Guid" ASC)
+)
+;
+CREATE TABLE T_Warehouse_Wafer (
+"Guid"  Guid NOT NULL,
+PRIMARY KEY ("Guid" ASC)
+)
+;
 Update T_System_Settings SET Value='1.6.3' WHERE ID=2;
