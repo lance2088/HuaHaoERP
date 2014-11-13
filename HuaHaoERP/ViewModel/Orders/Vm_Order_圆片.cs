81395b0d2d723fc53531dd5d26c4fda74fe4a2d7
HuangShuhang_i5 <yhnbgfd@gmail.com>
2014-11-13 15:01:09 +0800

    + ViewModel class

---
+++ HuaHaoERP/ViewModel/Orders/Vm_Order_圆片.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.ViewModel.Orders
+{
+    class Vm_Order_圆片
+    {
+    }
+}
---
+++ HuaHaoERP/ViewModel/Warehouse/Vm_Warehouse_圆片.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuaHaoERP.ViewModel.Warehouse
+{
+    class Vm_Warehouse_圆片
+    {
+    }
+}
