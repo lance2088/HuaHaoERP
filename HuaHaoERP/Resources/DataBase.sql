cdb53c9bbbbafaddc8dd16c00fd8bb3d5f08647c
HuangShuhang_i5 <yhnbgfd@gmail.com>
2014-11-14 17:23:03 +0800

    v1.6.6

--- HuaHaoERP/Resources/DataBase.sql
+++ HuaHaoERP/Resources/DataBase.sql
@@ -442,3 +442,7 @@ PRIMARY KEY ("Guid" ASC)
 )
 ;
 Update T_System_Settings SET Value='1.6.4' WHERE ID=2;
+alter TABLE T_Warehouse_Wafer ADD "LossQuantity" INTEGER;
+alter TABLE T_Warehouse_Wafer ADD "HalfProductGuid" Guid;
+alter TABLE T_Warehouse_Wafer ADD "HalfProductQuantity" INTEGER;
+Update T_System_Settings SET Value='1.6.5' WHERE ID=2;
